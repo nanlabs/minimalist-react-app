f9850eb94cbac929b297cb41cd6fe7829137f7c2
matiasalvarez87 <matiasalvarez87@gmail.com>
2018-02-17 18:53:52 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+
+const App = () => (
+  <div>
+    <h1>Minimalist React Webapp</h1>
+    <img width="50px" height="50px" src={logo} />
+  </div>
+);
+
+export default App;
